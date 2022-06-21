75c47fc423254b2437f20723a350422646644487
Burak Ferit Aktan <61063293+burakaktan@users.noreply.github.com>
2022-06-21 18:01:00 +0300

    Create short.cpp

---
+++ 820. Short Encoding of Words/short.cpp
@@ -0,0 +1,61 @@
+/*
+    Time Complexity: O(total number of characters in the input)
+    Space Complexity: O(total number of characters in the input)
+    
+    Main idea:
+        Add reverse of input strings to a Trie
+        answer is sum(depth+1) for all leaf nodes in the Trie
+        (+1 is to count # signs)
+    */
+class Solution {
+public:
+    typedef struct Trie
+    {
+        typedef struct TrieNode
+        {
+            unordered_map<char,TrieNode*> children;
+        }TrieNode;
+        TrieNode *root;
+        unordered_map<TrieNode*,int> nodes; // add <node,depth> pairs
+        Trie()
+        {
+            root = new TrieNode();
+            nodes[root] = 0;
+        }
+        void reverseAdd(const string &s)
+        {
+            TrieNode *curr = root;
+            for(int i = s.size()-1;i > -1;i--)
+            {   
+                if(curr->children[s[i]] == nullptr)
+                {
+                    curr->children[s[i]] = new TrieNode();
+                    nodes[curr->children[s[i]]] = s.size() - i;
+                }
+                
+                curr = curr-> children[s[i]];
+                
+            }
+        }
+    }Trie;
+    
+    int minimumLengthEncoding(vector<string>& words) {
+        Trie t;
+        // add reverse of each input string to the Trie
+        for(string &w : words)
+        {
+            t.reverseAdd(w);
+        }
+        
+        int ans = 0;
+        
+        for(auto it : t.nodes)
+        {
+            // if node is a leaf node
+            if(it.first->children.size() == 0)
+                // increment answer by depth of that leaf node + 1
+                ans += it.second + 1;
+        }
+        return ans;
+    }
+};
