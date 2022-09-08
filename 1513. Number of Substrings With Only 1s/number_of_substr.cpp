1ca691516752942dff640c916dd257e188738f83
Burak Ferit Aktan <61063293+burakaktan@users.noreply.github.com>
2022-09-08 12:52:57 +0300

    Update number_of_substr.cpp

--- 1513. Number of Substrings With Only 1s/number_of_substr.cpp
+++ 1513. Number of Substrings With Only 1s/number_of_substr.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     /*
-    we'll interpret the input as a gruop of (number_of_consequent_occurrence,number) pairs 
+    let's interpret the input as a gruop of (number_of_consequent_occurrence,number) pairs 
     011110001 -> one 0, four 1, three 0, one 1
     answer is sum_of(n*(n+1)/2) % mod for all n before 1s
+    instead of adding n*(n+1)/2 at the end of the batch, we can add them while we iterate through the batch, like:
+    at first element of the batch increment ans by 1
+    at second element of the batch increment ans by 2
+    and so on...
     Time Complexity: o(n)
     Space Complexity: O(1)
     where n is size of the input string
